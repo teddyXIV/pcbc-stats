b149cc3c08e65ae734ce97d894a2905e9821314e
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-03-28 13:50:00 -0700

    add logout front end + backend

---
+++ components/logout.tsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+import router from "next/router";
+
+const Logout = () => {
+    const logout = async () => {
+        try {
+            await axios.get('api/users/logout');
+            router.push('/login')
+        } catch (error: any) {
+            console.log(error.message)
+        }
+    }
+
+    return (
+        <button onClick={logout}>Logout</button>
+    )
+}

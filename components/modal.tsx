5f79e59c0b3234015badf6ee45063b87af766951
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-04-03 10:25:55 -0700

    add modal to confirm delete

---
+++ components/modal.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+const Modal = () => {
+    return (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+            <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
+                <div className="text-center">
+                    <h3 className="text-2xl font-bold text-gray-900">Are you sure you want to delete this game?</h3>
+                    <div>
+                        <Link href="/your-stats">No</Link>
+                        <Link href="/">Yes, delete</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
